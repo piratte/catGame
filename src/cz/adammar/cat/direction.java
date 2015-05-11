63b5008f4d998fd22f0720d71dc0b56a66ca069d
madam <madam@coulomb.farm.particle.cz>
2015-05-11 14:24:32 +0200

    added delta

--- src/cz/adammar/cat/direction.java
+++ src/cz/adammar/cat/direction.java
@@ -6,5 +6,23 @@ package cz.adammar.cat;
  *
  */
 public enum direction {
-	UP, DOWN, LEFT, RIGHT;
+	UP(0,1), 
+	DOWN(0,-1), 
+	LEFT(-1,0), 
+	RIGHT(1,0);
+	
+	private int dX, dY;
+	
+	private direction(int dX, int dY){
+		this.dX = dX;
+		this.dY = dY;
+	}
+	
+	public int getXDelta(){
+		return dX;
+	}
+	
+	public int getYDelta(){
+		return dY;
+	}
 }
