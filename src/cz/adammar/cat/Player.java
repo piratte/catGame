980c4708c2be6c354b745e6757dd0e826a0f3094
madam <madam@coulomb.farm.particle.cz>
2015-05-11 16:06:12 +0200

    set and update direction methods

--- src/cz/adammar/cat/Player.java
+++ src/cz/adammar/cat/Player.java
@@ -3,21 +3,24 @@ package cz.adammar.cat;
 public class Player extends Beast {
 	
 	private final direction DEF_DIR = direction.RIGHT;
-
 	
-	public Player(int x, int y, ImageLoader imgLoader, Maze maze) {
-		this.x = x; this.y = y;
+	private direction nextDir;
+
+	public Player(int x, int y, int speed, ImageLoader imgLoader, Maze maze) {
+		this.x = x; this.y = y; this.speed = speed;
 		left = imgLoader.loadImage("catLeft.png");
 		right = imgLoader.loadImage("catRight.png");
 		up = imgLoader.loadImage("catUp.png");
 		down = imgLoader.loadImage("catDown.png");
-		dir = DEF_DIR;
+		dir = DEF_DIR;		
 	}
 	
-	@Override
-	public void move() {
-		// TODO Auto-generated method stub
-
+	public void setDirection(direction next){
+		nextDir = next;
+	}
+	
+	public void updateDirection() {
+		dir = nextDir;
 	}
 
 }
