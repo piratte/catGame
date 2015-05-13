b44f7dececccc279dcb1e94bf69e86f121ee2078
madam <madam@coulomb.farm.particle.cz>
2015-05-13 14:21:04 +0200

    added getOpposite function and comments

--- src/cz/adammar/cat/direction.java
+++ src/cz/adammar/cat/direction.java
@@ -18,10 +18,36 @@ public enum direction {
 		this.dY = dY;
 	}
 	
+	/**
+	 * Get the opposite direction for input
+	 * @param d input direction
+	 * @return opposite to the input direction
+	 */
+	public direction getOpposite(direction d){
+		switch(d){
+			case UP:
+				return DOWN;
+			case DOWN:
+				return UP;
+			case LEFT:
+				return RIGHT;
+			default: // = RIGHT (switch in this case has to have a default
+				return LEFT;
+		}
+	}
+	
+	/**
+	 * Get the coefficient to x coordinate, when traveling this direction
+	 * @return 1 if going down, -1 if going up, 0 otherwise
+	 */
 	public int getXDelta(){
 		return dX;
 	}
 	
+	/**
+	 * Get the coefficient to x coordinate, when traveling this direction
+	 * @return 1 if going right, -1 if going left, 0 otherwise
+	 */
 	public int getYDelta(){
 		return dY;
 	}
